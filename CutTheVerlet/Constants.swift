8d967ba503723b52f3b4e7087870a71cbc48b6da
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-10-29 23:18:54 +1100

    Init: Starter Project

---
+++ CutTheVerlet/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CutTheVerlet
+//
+//  Created by Nick Lockwood on 14/09/2014.
+//  Copyright (c) 2014 Nick Lockwood. All rights reserved.
+//
+
+import UIKit
+
+//MARK: constants
+
+
+
+//MARK: game configuration
+
---
+++ CutTheVerlet/GameScene.swift
@@ -0,0 +1,105 @@
+//
+//  GameScene.swift
+//  CutTheVerlet
+//
+//  Created by Nick Lockwood on 07/09/2014.
+//  Copyright (c) 2014 Nick Lockwood. All rights reserved.
+//
+
+import SpriteKit
+import AVFoundation
+
+
+
+class GameScene: SKScene {
+    
+    
+
+    override func didMoveToView(view: SKView) {
+        
+        setUpPhysics()
+        setUpScenery()
+        setUpPrize()
+        setUpRopes()
+        setUpCrocodile()
+        
+        setUpAudio()
+    }
+    
+    //MARK: Level setup
+    
+    private func setUpPhysics() {
+        
+
+    }
+    
+    private func setUpScenery() {
+        
+
+    }
+    
+    private func setUpPrize() {
+        
+
+    }
+    
+    //MARK: Rope methods
+    
+    private func setUpRopes() {
+        
+
+    }
+    
+    //MARK: Croc methods
+    
+    private func setUpCrocodile() {
+        
+
+    }
+    
+    private func animateCrocodile() {
+        
+
+    }
+    
+    private func runNomNomAnimationWithDelay(delay: NSTimeInterval) {
+
+        
+    }
+    
+    //MARK: Touch handling
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+
+        
+    }
+    
+    //MARK: Game logic
+    
+    override func update(currentTime: CFTimeInterval) {
+        
+
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+
+
+    }
+    
+    private func checkIfRopeCutWithBody(body: SKPhysicsBody) {
+        
+        
+    }
+    
+    private func switchToNewGameWithTransition(transition: SKTransition) {
+        
+        
+    }
+    
+    //MARK: Audio
+    
+    private func setUpAudio() {
+        
+        
+    }
+}
